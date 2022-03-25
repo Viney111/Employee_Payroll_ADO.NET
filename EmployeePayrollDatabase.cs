45df4ab89a27178fda55f95ba10ac48612cbd8c6
Viney <vineykhaneja999@gmail.com>
2022-03-25 16:55:37 +0530

    Add project files.

---
+++ EmployeePayrollDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Employee_Payroll_ADO.NET
+{
+    internal class EmployeePayrollDatabase
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll_ADO.NET
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Database");
+        }
+    }
+}
