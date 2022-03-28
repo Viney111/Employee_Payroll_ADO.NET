c885f133d990492502f048e0362c323994d4ec45
Viney <vineykhaneja999@gmail.com>
2022-03-28 23:19:28 +0530

    [Viney] ADDED: Adding Employee Data from Database with ADO.NET

--- Program.cs
+++ Program.cs
@@ -13,6 +13,21 @@ namespace Employee_Payroll_ADO.NET
             Console.WriteLine("Welcome to Employee Payroll Database");
             EmployeePayrollDatabase employeePayrollDatabase = new EmployeePayrollDatabase();
             employeePayrollDatabase.GetAllEmployeeRecords();
+            Employee employee = new Employee
+            {
+                employeeName = "RamSingh",
+                gender = "M",
+                phoneNo = 9991661664,
+                employeeAddress = "VPO Kotputli, Rajasthan",
+                startDate = DateTime.Now,
+                basicPay = 1200000,
+                deductions = 200000,
+                incomeTax = 100000,
+                companySelect = 1,
+                employeeSelect = 5,
+                departmentSelect = 1,
+            };
+            //employeePayrollDatabase.AddEmployeeToDatabase(employee);
         }
     }
 }
